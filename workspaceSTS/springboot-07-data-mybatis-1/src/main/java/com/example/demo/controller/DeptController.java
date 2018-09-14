1c53823c6585e0fdfad74890394e713ed4e90038
ex-caowu <ex-caowu@DS-TS-0490.dashuf.local>
2018-09-14 10:10:30 +0800

    commit 2018/9/14
    Change-Id: Ie0f284ffa0a64cee2a309d9905570c51ee3c3dbe
    Signed-off-by: ex-caowu <ex-caowu@DS-TS-0490.dashuf.local>

--- workspaceSTS/springboot-07-data-mybatis-1/src/main/java/com/example/demo/controller/DeptController.java
+++ workspaceSTS/springboot-07-data-mybatis-1/src/main/java/com/example/demo/controller/DeptController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,32 +12,40 @@ import com.example.demo.bean.Employee;
 import com.example.demo.enums.Commons;
 import com.example.demo.mapper.DepartmentMapper;
 import com.example.demo.mapper.EmployeeMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.skyline.common.entity.Department;
 
 @RestController
 public class DeptController {
-	
+
 	@Autowired
 	DepartmentMapper departmentMapper;
 	@Autowired
 	EmployeeMapper employeeMapper;
-	
+
 	@GetMapping("/dept/{id}")
-	public Department getDepartment(@PathVariable("id") Integer id) {
+	public PageInfo<Department> getDepartment(@PathVariable("id") Integer id) {
 		System.out.println(Commons.SUFCOINNAME.getType());
-		return departmentMapper.selectByPrimaryKey(id);
+		PageHelper.offsetPage(0, 5);
+		PageHelper.orderBy("id ASC");
+		List<Department> list = departmentMapper.selectAll();
+		PageInfo<Department> pageInfo = new PageInfo<>(list,5);
+		
+		return pageInfo;
+
 	}
-	
+
 	@GetMapping("/dept")
 	@Transactional
 	public Department insertDept(Department department) {
 		departmentMapper.insertDept(department);
 		return department;
 	}
- 
+
 	@GetMapping("/emp/{id}")
 	public Employee getEmp(@PathVariable("id") Integer id) {
 		return employeeMapper.getEmpById(id);
-		
+
 	}
 }
